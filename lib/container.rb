be063a715278c52f3b60955028363c711cef0ef8
mikecalex <mikecalex@gmail.com>
2017-12-10 20:16:07 -0500

    init commit

---
+++ lib/container.rb
@@ -0,0 +1,35 @@
+require 'pry'
+class Container
+  attr_reader :weight, :maximum_holding_weight
+  attr_accessor :ingredients
+
+  def initialize(weight, maximum_holding_weight)
+    @weight = weight
+    @maximum_holding_weight = maximum_holding_weight
+    @ingredients = []
+  end
+
+  def fill_with_ingredient(ingredient)
+    number_of_ing = @maximum_holding_weight / ingredient.weight
+    number_of_ing.to_i.times { @ingredients << ingredient }
+    @weight += number_of_ing * ingredient.weight
+    which_ingredient
+  end
+
+  def which_ingredient
+	 @ingredients[0].name
+	end
+
+  def how_many_ingredients
+    @ingredients.length
+  end
+
+  def remove_one_ingredient
+    @ingredients.pop
+  end
+
+  def empty
+    @ingredients.clear
+  end
+
+end
